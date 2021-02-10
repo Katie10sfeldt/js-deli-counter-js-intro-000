a31635b062cda6f824959ff59b16b7b357d8e10d
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 17:48:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber(currentLine, newCustomer) {
+  currentLine.push(newCustomer);
+  return `Welcome, ${newCustomer}. You are number ${currentLine.length + 1} in line.`;
+}
+
+function nowServing(currentLine) {
+  if (currentLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+  else {
+    return `Currently serving ${currentLine[0]}`
+  }
+}
