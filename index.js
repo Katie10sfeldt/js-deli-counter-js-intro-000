5bee0e2b37e4b8d66243fce171f5e5b9d0535ae7
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 18:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
+  var list = [];
   if (line.length === 0) {
     return 'The line is currently empty.'
-  } else {
-    var list = [];
-    for (let i = 0; i < line.length; i++) {
-      list.push(`${i + 1}. ${line[i]}`);
-      return list;
-    }
-    return `The line is currently: ${[...list]}`;
   }
+  for (let i = 0; i < line.length; i++) {
+    list.push(`${i + 1}. ${line[i]}`);
+    return list;
+    }
+  return `The line is currently: ${[...list]}`;
+
 }
