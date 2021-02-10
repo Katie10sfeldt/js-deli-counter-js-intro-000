fdd59da5c85ab3edf4845a7e255597b991ec5261
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 17:50:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currentLine, newCustomer) {
   currentLine.push(newCustomer);
-  return `Welcome, ${newCustomer}. You are number ${currentLine.length + 1} in line.`;
+  return `Welcome, ${newCustomer}. You are number ${currentLine.length} in line.`;
 }
 
 function nowServing(currentLine) {
